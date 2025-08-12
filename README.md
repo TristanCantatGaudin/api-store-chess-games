9ea21d314eb7449918d977cb005e632feeb8b287
TristanCantatGaudin <tristancantat@gmail.com>
2025-08-12 14:14:02 +0200

    nonpersist POST works with pydantic

---
+++ README.md
@@ -0,0 +1,9 @@
+Start with: `uv run uvicorn main:app --reload`
+
+## In Browser
+
+Verify it is running at http://127.0.0.1:8000/
+
+Documentation at http://127.0.0.1:8000/redoc
+
+Swagger UI at http://127.0.0.1:8000/docs
--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-def main():
-    print("Hello from api-store-chess-games!")
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+all_games = []
+
+# Use a pydantic object instead of a basic Python dictionary.
+class GameItem(BaseModel):
+    lichessId: str = None # optional
+    PGN: str # NOT optional!
+    White: str = None
+    Black: str = None
+    WhiteElo: int = None
+    BlackElo: int = None
+    Opening: str = None
 
 
-if __name__ == "__main__":
-    main()
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"Hello": "World"}
+
+# Appending (regardless of whether the item already exists) is a POST request.
+@app.post("/games", response_model=list[GameItem])
+def add_game_nonpersist(item: GameItem) -> list[GameItem]:
+    """
+    For testing. Adds games to a temporary list. Storage does not persist over restarts.
+    The input has to be passed as a JSON payload.
+    Returns the full list (including the last game added).
+    """
+    all_games.append(item)
+    return all_games
